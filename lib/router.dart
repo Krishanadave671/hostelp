e236c7784da438a18e366c6e5206ce1b879c67a6
Krishanadave671 <84977709+Krishanadave671@users.noreply.github.com>
2023-02-10 11:57:47 +0530

    refactor : code refactored

--- lib/router.dart
+++ lib/router.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hostelp/features/Hostel_list/hostel_list.dart';
-import 'package:hostelp/features/auth/screens/login_screen.dart';
-import 'package:hostelp/features/home/screens/home_screen.dart';
-import 'package:hostelp/features/profilepage/screens/profile_page.dart';
 import 'package:hostelp/features/search/screens/search_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(routes: {
-  '/': (_) => const MaterialPage(child: LoginScreen()),
+  '/': (_) => const MaterialPage(child: SearchScreen()),
 });
 
 final loggedInRoute = RouteMap(routes: {
-  '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/': (_) => const MaterialPage(child: SearchScreen()),
   '/search': (_) => const MaterialPage(child: SearchScreen()),
 });
 
