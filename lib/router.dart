eee8df3251de19aeb1fbd5238f84392d0f4d16aa
Arihant Tonage <arihanttonage@gmail.com>
2023-02-10 13:34:28 +0530

    import statement removed from router.dart

--- lib/router.dart
+++ lib/router.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hostelp/features/Hostel_list/hostel_list.dart';
 import 'package:hostelp/features/auth/screens/login_screen.dart';
 import 'package:hostelp/features/home/screens/home_screen.dart';
-import 'package:hostelp/features/profilepage/screens/profile_page.dart';
 import 'package:hostelp/features/search/screens/search_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
