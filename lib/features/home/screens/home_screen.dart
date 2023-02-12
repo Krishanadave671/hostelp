35860461a7958ae9f0ddb98a9d0764e67a2ab61c
devout-coder <awessssomepro@gmail.com>
2023-02-12 23:12:13 +0530

    the final final final one

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -23,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    // double height = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
         extendBodyBehindAppBar: true,
