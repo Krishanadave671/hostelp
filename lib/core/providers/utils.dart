a628273915de51b1177dc26bb1ec1d158b6d2a24
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2023-03-07 14:39:04 +0530

    Added functionalities to home repository

---
+++ lib/core/providers/utils.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+class Utils {
+
+   static double degrees(double radians) {
+        return radians * 180 / pi;
+    }
+
+   static double radians(double degrees) {
+        return degrees * pi / 180;
+    }
+}
